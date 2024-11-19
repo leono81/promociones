bf4c63f5c96f42c961909f05070e5e3612fb1d39
Leandro Terrado <lterrado@gmail.com>
2024-11-19 01:15:30 -0300

    se corrige el endpoint del backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configura la URL base de la API
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", // Reemplaza con tu URL real
+  baseURL: "https://api-promociones.onrender.com", // Reemplaza con tu URL real
 });
 
 export default api;
